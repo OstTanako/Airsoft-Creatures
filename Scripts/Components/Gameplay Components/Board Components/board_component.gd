220609c003034f8b0abef0797d468c36884deeb6
OstTanako <theyllerbhrayhan@gmail.com>
2026-03-25 15:59:51 -0300

    bruh

--- Scripts/Components/Gameplay Components/Board Components/board_component.gd
+++ Scripts/Components/Gameplay Components/Board Components/board_component.gd
@@ -6,21 +6,26 @@ class_name BoardComponent
 @export var board : Node2D
 @export var limit_area : Area2D
 @export var warning_area : Area2D
-@export var board_player : AnimationPlayer
-@export var warning_icon : Sprite2D
+@export var limit_progress : ProgressBar
 
 @export var board_animator : BoardAnimatorComponent
 
+var invasion_delay = Timer.new()
 
 ## Initial Setup.
 func _ready() -> void:
 	_setup()
 
+func _process(_delta: float) -> void:
+	_update_progress()
+
 ## Component Functions.
 func _limit_invaded(body : Node2D) -> void:
 	if body.is_in_group("Intruder"):
-		print("Intruder invaded the limit line so.. GAME OVER!")
-		get_tree().quit()
+		invasion_delay.start()
+		
+		#print("Intruder invaded the limit line so.. GAME OVER!")
+		#get_tree().quit()
 
 func _in_danger(body : Node2D):
 	if body.is_in_group("Intruder"):
@@ -32,8 +37,26 @@ func _now_safe(body : Node2D) -> void:
 		#board_animator._scale_board(Vector2(1.0, 1.0))
 		pass
 
+func _update_progress() -> void:
+	limit_progress.max_value = invasion_delay.wait_time
+	limit_progress.value = invasion_delay.time_left
+
+func _invasion_complete() -> void:
+	get_tree().quit()
+
+func _invasion_failed(body : Node2D) -> void:
+	if body.is_in_group("Intruder"):
+		invasion_delay.stop()
+
 func _setup():
 	limit_area.body_entered.connect(_limit_invaded)
+	limit_area.body_exited.connect(_invasion_failed)
 	
 	warning_area.body_entered.connect(_in_danger)
 	warning_area.body_exited.connect(_now_safe)
+	
+	invasion_delay.wait_time = 0.7
+	invasion_delay.one_shot = true
+	invasion_delay.timeout.connect(_invasion_complete)
+	
+	add_child(invasion_delay)
